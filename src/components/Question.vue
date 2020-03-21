bb5ea146ab40af845b5e1bd39dc84c4837bbdeae
Aaron Shekey <aaron@aaronshekey.com>
2020-03-21 18:22:09 -0500

    Some conditional stuff

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -4,21 +4,42 @@
       <div class="grid gs8 jc-center sm:jc-start fc-light">
         <div class="grid fd-column ai-center px12 py6 sm:fd-row sm:p0">
           <div class="grid--cell fs-body3 sm:mr4">{{ vote_count }}</div>
-          <div class="grid--cell fs-fine">votes</div>
+          <div class="grid--cell fs-fine">
+            <span v-if="vote_count === 1 || vote_count === -1">
+              vote
+            </span>
+            <span v-else>
+              votes
+            </span>
+          </div>
         </div>
-        <div class="grid fd-column ai-center px12 py6 sm:fd-row sm:pt2 sm:pb2 sm:pl6 sm:pr6" v-bind:class="{ 'bg-green-500 fc-white bar-sm': is_answered }">
+        <div class="grid fd-column ai-center px12 py6 sm:fd-row sm:pt2 sm:pb2 sm:pl6 sm:pr6 bar-sm" v-bind:class="{ 'bg-green-500 fc-white bc-transparent': is_answered, 'ba bc-green-2 fc-green-500': hasAnswers }">
           <div class="grid--cell fs-body3 sm:mr4">{{ answer_count }}</div>
-          <div class="grid--cell fs-fine">answers</div>
+          <div class="grid--cell fs-fine">
+            <span v-if="answer_count === 1">
+              answer
+            </span>
+            <span v-else>
+              answers
+            </span>
+          </div>
         </div>
         <div class="grid fd-column ai-center px12 py6 sm:fd-row sm:p0">
           <div class="grid--cell fs-body3 sm:mr4">{{ view_count }}</div>
-          <div class="grid--cell fs-fine">views</div>
+          <div class="grid--cell fs-fine">
+            <span v-if="view_count === 1">
+              view
+            </span>
+            <span v-else>
+              views
+            </span>
+          </div>
         </div>
       </div>
     </div>
     <div class="grid--cell fl1 sm:w100">
-      <a v-bind:href=url class="s-link fs-subheading d-inline-block mb4 sm:mb2">{{ title }}</a>
-      <p class="v-truncate2 mb8" v-if="body !== undefined">{{ body }}</p>
+      <a v-bind:href=url class="s-link fs-subheading d-inline-block mb4 sm:mb8" v-bind:class="{ 'sm:mb2': hasBody }">{{ title }}</a>
+      <p class="v-truncate2 mb8" v-if="hasBody">{{ body }}</p>
       <div class="grid gs4 ai-center fw-wrap">
         <a class="grid--cell s-tag" href="#">git</a>
         <a class="grid--cell s-tag" href="#">version-control</a>
@@ -51,6 +72,22 @@ export default {
     display_name: String,
     profile_url: String,
     is_answered: Boolean
+  },
+  computed: {
+    hasBody: function () {
+      if (this.body === undefined) {
+        return false
+      } else {
+        return true
+      }
+    },
+    hasAnswers: function () {
+      if (this.answer_count > 0) {
+        return true
+      } else {
+        return false
+      }
+    }
   }
 }
 </script>
