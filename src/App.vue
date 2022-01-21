f6ab78433659504f373b9a273fe11a03d88ce5b0
Aaron Shekey <aaron@aaronshekey.com>
2022-01-20 18:08:55 -0600

    Give the page some styling

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <PostSummary v-for="item of items" :key="item.question_id"
-      :title=item.title
-      :view_count=item.view_count
-      :vote_count=item.score
-      :answer_count=item.answer_count
-      :url=item.link
-      :profile_url=item.owner.link
-      :profile_image=item.owner.profile_image
-      :reputation=item.owner.reputation
-      :display_name=item.owner.display_name
-      :is_answered=item.is_answered
-      :tags=item.tags
-      :last_activity_date=item.last_activity_date
-    />
+    <div class="bar-lg bg-white bs-md mx-auto wmx8">
+      <PostSummary v-for="item of items" :key="item.question_id"
+        :title=item.title
+        :view_count=item.view_count
+        :vote_count=item.score
+        :answer_count=item.answer_count
+        :url=item.link
+        :profile_url=item.owner.link
+        :profile_image=item.owner.profile_image
+        :reputation=item.owner.reputation
+        :display_name=item.owner.display_name
+        :is_answered=item.is_answered
+        :tags=item.tags
+        :last_activity_date=item.last_activity_date
+      />
+    </div>
   </div>
 </template>
 
